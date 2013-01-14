457f7dfa0879131fc75b3ce84b88032105a32205
poofytoo <victorhung92@gmail.com>
2013-01-14 01:32:26 -0500

    Init for Eye

--- thehans/laptop/eye.py
+++ thehans/laptop/eye.py
@@ -1,6 +1,14 @@
-import math
+import math, Image
+import cv2.cv as cv
 
 class Eye:
 
-    def __init__(self):
+    def __init__(self, debug=False):
+        if debug: print "Debugging mode for vision active"
+        self.camcapture = cv.CreateCameraCapture(1)
         
+    def getFrame(self):
+        frame = cv.QueryFrame(self.camcapture)
+        return frame
+    
+# cyclops = Eye(debug=True)
