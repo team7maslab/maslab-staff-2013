2801f1307ba0acd3ce66281bf5340abe1c412041
Hosea Siu <hoseasiu@mit.edu>
2013-01-25 11:23:53 -0500

    changed to accomodate navigation.py

--- thehans/laptop/main.py
+++ thehans/laptop/main.py
@@ -1,9 +1,10 @@
 import arduinoSimpleSerial as arduino
-import time, eye, random, sys, stateMachine
+import virtualBot
+import time, eye, random, sys, stateMachine, navigation, constants
 
-def run(state, ard):
+def run(state, ard, nav):
 
-    findColor = "green"
+    findColor = "red and green"
     
     #Waiting for button press on robot to denote start
     # need to add button press into the code
@@ -17,82 +18,89 @@ def run(state, ard):
 
     wallTOL = 70
 
-    runTime = 180
-    stopTime = time.time() + runTime
+#    stopTime = time.time() + constants.gameTime
+    stopTime = time.time() + 35
     timeLeft = stopTime - time.time()
     state.stopTime = stopTime
     state.nextState(stopTime)
 
-    sleepTime = 0.2
-    turnTime = 1/sleepTime
-    turnDir = random.randrange(-10, 10, 1)
+    # need to calibrate for full turn
+    turnEncCount = 120
     
     cyclop = eye.Eye(debug=True)
-    x = 0
-    y = 0
-    
+   
     try:
         while True:
 
             currentState = state.nextState(stopTime)
+            print currentState
+            frame = cyclop.getFrame()
 
-            irVals = ard.retrieve("I")
-            bumpVals = ard.retrieve("U")
-            print irVals
+            if (currentState == "end"):
+                ard.kill()
+                break
+
+            ###### insert actual IR retrieval methods here
+#            irVals = ard.retrieve("I")
+#            bumpVals = ard.retrieve("U")
+#            print irVals
 
             wallOnSide = "none"
 
             # check for collsions
-            if (bumpVals[1] == 1 or bumpVals[2] == 1 or irVals[1] > 500 or irVals[2] > 500 or irVals[3] > 500):
-                ard.motorCommand(-0.5)
-                ard.turnCommand(-0.5)
-                ard.packetExchange()
-                ard.motorCommand(-0.5)
-                ard.turnCommand(0.5)
-                ard.packetExchange()
+
+
+##            if (bumpVals[1] == 1 or bumpVals[2] == 1 or irVals[1] > 500 or irVals[2] > 500 or irVals[3] > 500):
+##                nav.backUpFromHit(ard)
+
+
+##                ard.motorCommand(-0.5)
+##                ard.turnCommand(-0.5)
+##                ard.packetExchange()
+##                ard.motorCommand(-0.5)
+##                ard.turnCommand(0.5)
+##                ard.packetExchange()
             
             if (currentState == "wallFollow"):
+                # need to be able to get IR values
+                
                 # not near wall
-                if (irVals[1] < 200 or irVals[2] < 200 or irVals[3] < 200):
-                    wallOnSide = "none"
-                    ard.motorCommand(0.5)
-                    ard.packetExchange()
-
-                # perpendicular to wall
-                elif (irVals[1] > 200 or irVals[1] > 220 or irVals[1] > 200):
-                    ard.motorCommand(-0.5)
-                    ard.turnCommand(0.5)
-                    ard.packetExchange()
-
-                # we are parallel to a wall
-                elif (irVals[1] > 250 and irVals[2] < 200):
-                    wallOnSide = "left"
-                    ard.motorCommand(0.5)
-                    ard.packetExchange()
-                    
-                elif (irVals[3] > 250 and irVals[2] < 200):
-                    wallOnSide = "right"
-                    ard.motorCommand(0.5)
-                    ard.packetExchange()
-
-                elif (wallOnSide == "left" and (250-irVals[1]) > wallTOL):
-                    ard.motorCommand(0.1)
-                    ard.turnCommand(-0.2)
-                    ard.packetExchange()
-                    
-                elif (wallOnSide == "right" and (250-irVals[3]) > wallTOL):
-                    ard.motorCommand(0.1)
-                    ard.turnCommand(0.2)
-                    ard.packetExchange()
-                                        
+##                if (irVals[1] < 200 or irVals[2] < 200 or irVals[3] < 200):
+##                    wallOnSide = "none"
+##                    ard.motorCommand(0.5)
+##                    ard.packetExchange()
+##
+##                # perpendicular to wall
+##                elif (irVals[1] > 200 or irVals[1] > 220 or irVals[1] > 200):
+##                    ard.motorCommand(-0.5)
+##                    ard.turnCommand(0.5)
+##                    ard.packetExchange()
+##
+##                # we are parallel to a wall
+##                elif (irVals[1] > 250 and irVals[2] < 200):
+##                    wallOnSide = "left"
+##                    ard.motorCommand(0.5)
+##                    ard.packetExchange()
+##                    
+##                elif (irVals[3] > 250 and irVals[2] < 200):
+##                    wallOnSide = "right"
+##                    ard.motorCommand(0.5)
+##                    ard.packetExchange()
+##
+##                elif (wallOnSide == "left" and (250-irVals[1]) > wallTOL):
+##                    ard.motorCommand(0.1)
+##                    ard.turnCommand(-0.2)
+##                    ard.packetExchange()
+##                    
+##                elif (wallOnSide == "right" and (250-irVals[3]) > wallTOL):
+##                    ard.motorCommand(0.1)
+##                    ard.turnCommand(0.2)
+##                    ard.packetExchange()
+                time.sleep(0.5)                                   
                 # need some PID control here
 
-                frame = cyclop.getFrame()
-                if (findColor == "green"):
-                    (x,y), frame = cyclop.findGreenBall(frame)
-                else:
-                    (x,y), frame = cyclop.findRedBall(frame)
-                if (x != 0 and y != 0):
+                (x,y), frame, radius = cyclop.findColor(frame, findColor)
+                if (x != -1.0 and y != -1.0):
                     state.ballFound = True
                 else:
                     state.ballFound = False
@@ -101,21 +109,19 @@ def run(state, ard):
                 
             elif (currentState == "explore"):
                 
-                frame = cyclop.getFrame()
-                if (findColor == "green"):
-                    (x,y), frame = cyclop.findGreenBall(frame)
-                else:
-                    (x,y), frame = cyclop.findRedBall(frame)
+                (x,y), frame, radius = cyclop.findColor(frame, findColor)
 
-                # if no ball is found
-                if (x == 0 and y == 0):
+                # if no ball is found- turn around
+                ##### need to make this go to furthest area
+                if (x == -1.0 and y == -1.0):
                     state.ballFound = False
-                    ard.motorCommand(0.5)
-                    if (turnTime == 0):
-                        turnTime = 1/sleepTime
-                        turnDir = random.randrange(-10, 10, 1)/10.0
-                    ard.turnCommand(turnDir)
-                    turnTime -= 1
+                    ard.turnCommand(constants.ninetyDegTurn)
+                    time.sleep(0.1)
+                    turnEncCount += 1
+                    if (turnEncCount == 120):
+                        ard.motorCommand(constants.defaultSpeed)
+                        time.sleep(2)
+                        turnEncCount = 0
 
                 else:
                     state.ballFound = True
@@ -124,22 +130,28 @@ def run(state, ard):
 
 
             elif (currentState == "getBalls"):
-                ard.motorCommand(0.5)
-                ard.turnCommand(x)
-                cyclop.showImage(frame)
-                ard.packetExchange()
-                time.sleep(sleepTime)
-                state.prevState = "getBalls"
+                (x,y), frame, radius  = cyclop.findColor(frame, findColor)
+                if (x == -1.0 and y == -1.0):
+                    state.prevState = "getBalls"
+                    state.ballFound = False
+                else:
+                    ard.motorCommand(constants.defaultSpeed)
+                    ard.turnCommand(x)
+                    ard.packetExchange()
+                    time.sleep(1)
+                    state.prevState = "getBalls"
+                print "x,y", x, y
                 
             elif (currentState == "scoreTower"):
-                cyclop.findPurpleTower()
+                (x,y), frame, radius = cyclop.findColor(frame, "purple")
 
-                ard.motorCommand(0.5)
+                ard.motorCommand(constants.defaultSpeed)
                 ard.turnCommand(x)
-                cyclop.showImage(frame)
                 ard.packetExchange()
 
-                ########### use eye.py to figure out if the tower is close enough
+                #### correct this to actually use IRs
+                closeEnough = (radius > 45)
+
                 if (closeEnough):
                     armCommand(1)
                     time.sleep(5)
@@ -148,17 +160,21 @@ def run(state, ard):
 
             elif (currentState == "scoreWall"):
 
-                cyclop.findYellowWall()
-                ard.motorCommand(0.5)
+                (x,y), frame, radius = cyclop.findColor(frame, "yellow")
+                ard.motorCommand(constants.defaultSpeed)
                 ard.turnCommand(x)
-                cyclop.showImage(frame)
                 ard.packetExchange()
 
-                ########### use eye.py to figure out if the tower is close enough
+                #### correct this to actually use IRs
+                closeEnough = (radius > 45)
+
                 if (closeEnough):
-                    enemyCommand(1)
+                    ard.enemyCommand(1)
                     time.sleep(5)
-                    enemyCommand(0)
+                    ard.enemyCommand(0)
+            ## for debugging only
+#            time.sleep(500)
+            cyclop.showImage(frame)
             
     except KeyboardInterrupt:
         cyclop.kill()
@@ -172,13 +188,16 @@ def kill():
     
 if __name__ == '__main__': #called from the command line
 
-    ard = arduino.Arduino()
-    time.sleep(1) # wait for arduino to power up
-    ard.connect(debug=True)
+#    ard = arduino.Arduino()
+#    ard.connect(debug=True)
+    ard = virtualBot.VirtualBot()   # testing w/ virtualbot
+    nav = navigation.Navigation()
+    time.sleep(2) # wait for arduino to power up
+
     ard.helixCommand(1)
     ard.intakeCommand(1)
     state = stateMachine.State()
     
-    run(state, ard)
-    time.sleep(100)
+    nav.backUpFromHit(ard)    
+    run(state, ard, nav)
     kill()
